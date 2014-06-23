ae9d58db76674f149170f76d3a2553d07258dc5f
acmerfight <acmerfight@gmail.com>
2014-06-23 15:30:12 +0800

    add lib

--- lib_misc.erl
+++ lib_misc.erl
@@ -4,6 +4,8 @@
 -export([odds_and_evens/1]).
 -export([map/2]).
 -export([qsort/1]).
+-export([pythag/1]).
+-export([perms/1]).
 
 
 filter(P, [H|T]) -> 
@@ -36,3 +38,16 @@ qsort([Pivot|T]) ->
     qsort([X || X <- T, X < Pivot])
     ++ [Pivot] ++
     qsort([X || X <- T, X >= Pivot]).
+
+pythag(N) ->
+    [{A, B, C} || 
+        A <- lists:seq(1, N),
+        B <- lists:seq(1, N),
+        C <- lists:seq(1, N),
+        A + B + C =< N,
+        A*A + B*B =:= C*C
+    ].
+
+
+perms([]) ->[[]];
+perms(L) -> [[H|T] || H <- L, T <- perms(L--[H])].
