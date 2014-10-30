2035204c3e0edbb81fcf818a66b0e7216c89c14d
acmerfight <acmerfight@gmail.com>
2014-10-30 18:24:58 +0800

    add my_module

---
+++ excise/my_module.erl
@@ -0,0 +1,28 @@
+-module(my_module).
+-export([print/1]).
+-export([either_or_both/2]).
+-export([area/1]).
+
+print(Term) ->
+    io:format("The value is: ~p.~n", [Term]).
+
+either_or_both(A, B) ->
+    case {A, B} of
+        {true, B} when is_boolean(B) ->
+            true;
+        {A, true} when is_boolean(A) ->
+            true;
+        {false, false} ->
+            false
+    end.
+
+
+area(Shape) -> 
+    case Shape of
+        {circle, Radius} ->
+            Radius * Radius * math:pi();
+        {square, Side} ->
+            Side * Side;
+        {rec, Height, Width} ->
+            Height * Width
+    end.
