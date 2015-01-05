d4976adf50cba447743d42480101b14fe2dc27f6
acmerfight <acmerfight@gmail.com>
2015-01-05 08:44:39 +0800

    add insert_user insert_porject

--- excise/create_tables.erl
+++ excise/create_tables.erl
@@ -2,6 +2,8 @@
 
 
 -export([init_tables/0]).
+-export([insert_user/3]).
+-export([insert_project/2]).
 
 -record(user, {id, name}).
 -record(project, {title, description}).
@@ -13,3 +15,19 @@ init_tables() ->
     mnesia:create_table(contributor,
         [{type, bag}, {attributes, record_info(fields, contributor)}]).
 
+
+insert_user(Id, Name, ProjectTitles) when ProjectTitles =/= [] ->
+    User = #user{id = Id, name = Name},
+    Fun = fun() ->
+            mnesia:write(User),
+            lists:foreach(
+                fun(Title) ->
+                    [#project{title = Title}] = mnesia:read(project, Title),
+                    mnesia:write(#contributor{user_id = Id, project_title = Title})
+                end,
+                ProjectTitles)
+          end,
+    mnesia:transaction(Fun).
+
+insert_project(Title, Description) ->
+    mnesia:dirty_write(#project{title = Title, description = Description}).
