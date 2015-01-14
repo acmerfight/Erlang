0ac1c49a872efbbe8a65048bc0dab0fc53e5f07f
acmerfight <acmerfight@gmail.com>
2015-01-14 23:49:47 +0800

    add map pmap1

--- excise/pmap.erl
+++ excise/pmap.erl
@@ -1,5 +1,11 @@
 -module(pmap).
 -export([pmap/2]).
+-export([pmap1/2]).
+-export([map/2]).
+
+
+map(_, []) -> [];
+map(F, [H|T]) -> [F(H)|map(F, T)].
 
 pmap(F, L) ->
     S = self(),
@@ -16,3 +22,21 @@ pmap(F, L) ->
         end;
 gather([], _) ->
     [].
+
+pmap1(F, L) ->
+    S = self(),
+    Ref = erlang:make_ref(),
+    foreach(fun(I) ->
+                spawn(fun() -> do_f1(S, Ref, F, I) end)
+            end, L),
+    gather1(length(L), Ref, []).
+
+do_f1(Parent, Ref, F, I) ->
+    Parent ! {Ref, (catch F(I))}.
+
+gather1(0, _, L) -> L;
+gather1(N, Ref, L) ->
+    receive
+        {Ref, Ret} -> gather1(N-1, Ref, [Ret|L])
+    end.
+
